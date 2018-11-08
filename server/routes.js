714736759d31b8cb45fc2f9f8e76468a0500a9e1
Robin Guan <hello@robinguan.com>
2018-11-08 00:35:57 -0800

    Updated routes

--- server/routes.js
+++ server/routes.js
@@ -3,11 +3,11 @@ module.exports = function(app){
 	//GET: Retrieve all Tasks
     app.get('/tasks', controller.showall);
     //GET: Retrieve a Task by ID
-    app.get('/:id', controller.show);
+    app.get('/tasks/:id', controller.show);
 	//POST: Create a Task
-    app.post('/new', controller.create);
+    app.post('/tasks/new', controller.create);
     //PUT: Update a Task by ID
-    app.put('/:id', controller.update);
+    app.put('/tasks/:id', controller.update);
     //DELETE: Delete a Task by ID
-    app.delete('/:id', controller.delete);
+    app.delete('/tasks/:id', controller.delete);
 }
